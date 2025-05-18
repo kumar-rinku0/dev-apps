1e4ff9bee0e97005766874bbd6a586e8fa99f93c
rinku kumar <rinkukumar412616@gmail.com>
2025-05-18 16:19:11 +0530

    router init

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,14 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = 3000;
 
+// Importing the user router
+import userRouter from "./routers/user";
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
+
+app.use("/user", userRouter);
 
 app.get("/api", (req: Request, res: Response) => {
   res.json({ message: "Hello from the API!" });
